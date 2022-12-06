64b05965c0e52e8a9a67fdbb04591246caf5d454
Kobi S <kobi.shasha@gmail.com>
2022-12-06 14:35:55 +0200

    added price

--- koburger/site.js
+++ koburger/site.js
@@ -1,3 +1,4 @@
+const price = 2.99;
 const root = document.getElementById("root");
 
 const ban1 = document.createElement("div");
@@ -7,3 +8,10 @@ root.appendChild(ban1);
 const ban2 = document.createElement("div");
 ban2.className = "ban";
 root.appendChild(ban2);
+
+const hr = document.createElement("hr");
+root.appendChild(hr);
+
+const p = document.createElement("p");
+p.innerHTML = "cost is : " + price;
+root.appendChild(p);
